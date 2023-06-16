e85dbfc2252197995045a88fdc36a166062a139d
programarf <programarf@gmail.com>
2023-06-16 14:38:12 -0500

    prices

---
+++ js/services/product/getProductPrices.js
@@ -0,0 +1,22 @@
+const getProductPrices = async (productId, currency, rol) => {
+    console.log(productId, currency, rol);
+    try {
+      const url = `https://safetticustom.azurewebsites.net/api/Product/prices/${productId}/${currency}/${rol}` 
+      const reqQueryProducts = await fetch(url)
+      const data = await reqQueryProducts.json()
+      
+      if (reqQueryProducts.status == 403) {
+        console.log('Error 403');
+      } else if (reqQueryProducts.status == 500) {
+        console.log('Error 500. Ocurrió un error al procesar su solicitud.');
+      }
+  
+      return data
+    }
+    
+    catch(error) {
+      console.log('No se pudo traer los asesores', error);
+    }
+  }
+  
+  export default getProductPrices
