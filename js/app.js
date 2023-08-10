6805a006f4f83422f2eee8a3a18220a454c0a87e
programarf <programarf@gmail.com>
2023-08-10 11:30:06 -0500

    change discount

--- js/app.js
+++ js/app.js
@@ -20,13 +20,11 @@ class App {
     document.addEventListener('DOMContentLoaded', async () => {
       const url = new URL(window.location.href);
       const searchParams = new URLSearchParams(url.search);
-      const uid = searchParams.get('uid') || '19';
+      const uid = searchParams.get('uid') || '4';
       const resQueryUser = await getUser(uid);
       const login = new Login();
       login.setHash(uid);
       const storedHash = login.getStoredHash();
-      // console.log(storedHash);
-
       if (this.quotation) {
         const quotationContentList = this.quotation.querySelector('#quotation--content--list');
         quotationContentList.insertAdjacentHTML('afterbegin', '<img class="quotation--loading qnimage--auto" src="../img/icon/icon-spinner.gif">');
