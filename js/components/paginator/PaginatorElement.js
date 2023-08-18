15d7b48285919e08350720fdca819a7d5ed6a43c
programarf <programarf@gmail.com>
2023-08-18 11:10:50 -0500

    fix: popint in the total

--- js/components/paginator/PaginatorElement.js
+++ js/components/paginator/PaginatorElement.js
@@ -39,29 +39,84 @@ class PaginatorElement extends HTMLElement {
   }
 
   paginatorNumber(results, totalPages) {
-   
     const paginator = document.createElement('div');
-   /*  const pager = document.querySelector('.pager');
-    console.log(pager);
-    if(pager){
-      pager.remove()
-    } */
+    const loadingDivHtml = document.querySelector('.loading-message')
+    if (loadingDivHtml) {
+      loadingDivHtml.remove();
+    }
     paginator.classList.add('pager');
+  
+    const maxPagesToShow = 5; // Número máximo de botones a mostrar
+    const ellipsisThreshold = 5; // Cantidad de páginas antes de mostrar "..."
+    const currentPage = this.pageNumber;
+  
     if (results && totalPages > 1) {
-      for (let i = 1; i <= totalPages; i++) {
-        const pageButton = document.createElement('button');
-        pageButton.textContent = i;
-        pageButton.value = i;
-        pageButton.classList.add('item-pager');
-        if (i === 1) {
-          pageButton.classList.add('active');
+      if (totalPages <= maxPagesToShow) {
+        // Mostrar todos los botones
+        for (let i = 1; i <= totalPages; i++) {
+          const pageButton = document.createElement('button');
+          pageButton.textContent = i;
+          pageButton.value = i;
+          pageButton.classList.add('item-pager');
+          if (i === currentPage) {
+            pageButton.classList.add('active');
+          }
+          paginator.appendChild(pageButton);
         }
-        paginator.appendChild(pageButton);
+      } else {
+        // Mostrar el rango alrededor de la página actual
+        const rangeStart = Math.max(currentPage - Math.floor(maxPagesToShow / 2), 1);
+        const rangeEnd = Math.min(rangeStart + maxPagesToShow - 1, totalPages);
+  
+        // Mostrar el botón para ir a la primera página
+        const firstPageButton = document.createElement('button');
+        firstPageButton.textContent = '1';
+        firstPageButton.value = 1;
+        firstPageButton.classList.add('item-pager');
+        paginator.appendChild(firstPageButton);
+  
+        // Agregar el botón "..." si es necesario
+        if (rangeStart > ellipsisThreshold) {
+          const ellipsisStartButton = document.createElement('button');
+          ellipsisStartButton.textContent = '...';
+          ellipsisStartButton.disabled = true;
+          ellipsisStartButton.classList.add('item-pager', 'hidden');
+          paginator.appendChild(ellipsisStartButton);
+        }
+  
+        // Mostrar los botones de página dentro del rango
+        for (let i = rangeStart; i <= rangeEnd; i++) {
+          const pageButton = document.createElement('button');
+          pageButton.textContent = i;
+          pageButton.value = i;
+          pageButton.classList.add('item-pager');
+          if (i === currentPage) {
+            pageButton.classList.add('active');
+          }
+          paginator.appendChild(pageButton);
+        }
+  
+        // Agregar el botón "..." si es necesario
+        if (rangeEnd < totalPages - ellipsisThreshold + 1) {
+          const ellipsisEndButton = document.createElement('button');
+          ellipsisEndButton.textContent = '...';
+          ellipsisEndButton.disabled = true;
+          ellipsisEndButton.classList.add('item-pager', 'hidden');
+          paginator.appendChild(ellipsisEndButton);
+        }
+  
+        // Mostrar el botón para ir a la última página
+        const lastPageButton = document.createElement('button');
+        lastPageButton.textContent = totalPages;
+        lastPageButton.value = totalPages;
+        lastPageButton.classList.add('item-pager');
+        paginator.appendChild(lastPageButton);
       }
     }
+  
     document.querySelector('c-paginator').appendChild(paginator);
-  }
-
+  }  
+  
   async handlePageButtonClick(e) {
     if (e && e.target.tagName === 'BUTTON') { 
       const pagerItem = document.querySelectorAll('.pager .item-pager');
@@ -73,6 +128,7 @@ class PaginatorElement extends HTMLElement {
       this.loading();
       e.target.classList.add('active');
       try {
+        this.updatePaginator();
         const uid = localStorage.getItem('current')
         const data = await QuotationSearch(uid, e.target.value, this.pageSize, this.advisorId, this.clientName);
         this.pageNumberCallback(data.results);
@@ -96,6 +152,17 @@ class PaginatorElement extends HTMLElement {
     }
   }
   
+  updatePaginator() {
+    // Limpia el paginador actual
+    const paginatorElement = document.querySelector('c-paginator');
+    while (paginatorElement.firstChild) {
+      paginatorElement.removeChild(paginatorElement.firstChild);
+    }
+    console.log(this.results, this.totalPages);
+    // Vuelve a renderizar el paginador con el nuevo rango
+    this.paginatorNumber(this.results, this.totalPages);
+  }
+
   selectAdvisor() {
     const selectAdvisorId = document.querySelector('#advisors');
     if(selectAdvisorId){
@@ -120,7 +187,7 @@ class PaginatorElement extends HTMLElement {
       if(quotation.length === 0){
         const loadingDiv = document.createElement('div');
         const quotationContentListContainer = document.querySelector('#quotation--content--list');
-        loadingDiv.textContent = 'No';
+        loadingDiv.textContent = 'No hay resultados';
         loadingDiv.classList.add('loading-message')
         quotationContentListContainer.appendChild(loadingDiv);
       }
