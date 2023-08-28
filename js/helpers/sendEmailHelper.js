1eb024fe3ff10e3b8be90a03104fc95f055981e7
Christian Vargas <cristianvargas385@gmail.com>
2023-08-28 18:28:00 -0500

    update style send email

--- js/helpers/sendEmailHelper.js
+++ js/helpers/sendEmailHelper.js
@@ -2,15 +2,17 @@ import sendEmail from "../services/email/sendEmail.js";
 import nodeNotification from "./nodeNotification.js";
 
 const sendEmailHelper = (event, nodo) => {
-  nodo.addEventListener('click', (e) => {
-    e.preventDefault()
-    nodeNotification('Enviando...')
-    nodo.textContent = 'Enviando...'
-    nodo.classList.add('loading')
-    let urlEmail = event.getAttribute('href')
-    sendEmail(event, urlEmail, nodo)
+  event.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    if(ev.target === nodo) {
+      nodo.textContent = 'Enviando...'
+      nodo.nextElementSibling.src = '../../img/icon/icon-spinner.gif'
+      nodo.classList.add('loading')
+      let urlEmail = event.getAttribute('href')
+      sendEmail(event, urlEmail, nodo)
+    }
   })
-
+  
 }
 
 export default sendEmailHelper
