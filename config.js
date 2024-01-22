d1e69f21b6ff4309af42b70d2a07b330dbf67596
programarf <programarf@gmail.com>
2024-01-22 15:33:18 -0500

    fix: add text of help in the coti

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
-// export const config = {
-//     API_KEY_DEV: "", //prod
-//     API_KEY_PROD: "",
-//     // API_KEY_DEV: "https://safetticustom.azurewebsites.net",// local
-//     API_DEV_IMAGE: "https://dev-co-safetti-b2b.pantheonsite.io",// local
-// }
-
 export const config = {
-    // API_KEY_DEV: "", //prod
+    //API_KEY_DEV: "", //prod
     API_KEY_PROD: "",
     API_KEY_DEV: "https://safetticustom.azurewebsites.net",// local
     API_DEV_IMAGE: "https://dev-co-safetti-b2b.pantheonsite.io",// local
 }
-
