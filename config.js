0472b823c6e89256ad584478cf50478bb37cf992
programarf <programarf@gmail.com>
2024-09-11 12:40:48 -0500

    merge master

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const config = {
     //baseUrl: "", //prod
+    //baseUrl: "https://safetticustom.azurewebsites.net",// local
     baseUrl: "https://localhost:7070",// local
     API_DEV_IMAGE: "https://dev-co-safetti-b2b.pantheonsite.io",// local
 }
