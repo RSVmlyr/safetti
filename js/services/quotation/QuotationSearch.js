52b579e12f8a11c565cf5f55ae2b00de8920efa7
Wilber Rivera <wilber.rivera@vmlyr.com>
2025-01-14 18:45:04 -0500

    nuevo filtro estado de la cotización

--- js/services/quotation/QuotationSearch.js
+++ js/services/quotation/QuotationSearch.js
@@ -1,8 +1,8 @@
 import Fetch from "../Fetch.js"
 
-const QuotationSearch = async (uid, pageNumber, pageSize, advisorId, clientName) => {
+const QuotationSearch = async (uid, pageNumber, pageSize, advisorId, quoteStatus, clientName) => {
   try {
-    const response = await Fetch.get(`/api/Quotation/search/${uid}/${pageNumber}/${pageSize}/${advisorId}/${clientName}`)
+    const response = await Fetch.get(`/api/Quotation/search/${uid}/${pageNumber}/${pageSize}/${advisorId}/${quoteStatus}/${clientName}`)
 
     if (response.status === "error") {
       console.error(response.message);
